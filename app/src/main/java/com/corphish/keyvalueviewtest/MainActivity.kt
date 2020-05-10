8a349e5f2ad2f2e0905eb13557bc4a754e447996
corphish <d97.avinaba@gmail.com>
2020-05-10 19:02:06 +0530

    app: Demo single item adapter
    Signed-off-by: corphish <d97.avinaba@gmail.com>

--- app/src/main/java/com/corphish/keyvalueviewtest/MainActivity.kt
+++ app/src/main/java/com/corphish/keyvalueviewtest/MainActivity.kt
@@ -2,11 +2,19 @@ package com.corphish.keyvalueviewtest
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.corphish.widgets.ktx.adapters.Adapters
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val countryList = listOf("USA", "Spain", "Italy", "Russia", "France")
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = Adapters.singleItemAdapterWith(countryList)
     }
 
     val text: String
