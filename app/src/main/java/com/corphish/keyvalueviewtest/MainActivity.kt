e827642047136147204dcd963cd20395d920974b
corphish <d97.avinaba@gmail.com>
2020-05-10 17:57:19 +0530

    Remove BottomSheetAlertDialog
    Signed-off-by: corphish <d97.avinaba@gmail.com>

--- app/src/main/java/com/corphish/keyvalueviewtest/MainActivity.kt
+++ app/src/main/java/com/corphish/keyvalueviewtest/MainActivity.kt
@@ -2,17 +2,11 @@ package com.corphish.keyvalueviewtest
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.corphish.widgets.ktx.BottomSheetAlertDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        BottomSheetAlertDialog(this).apply {
-            title = "Jorgo"
-            message = "lahaptup"
-        }.show()
     }
 
     val text: String
