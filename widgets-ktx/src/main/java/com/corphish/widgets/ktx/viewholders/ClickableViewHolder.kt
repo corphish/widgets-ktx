031684dba6370f3c289b208a1f037831ef5038cc
corphish <d97.avinaba@gmail.com>
2020-05-11 12:00:50 +0530

    ClickableViewHolder: Convert onClickListener to anonymous function
    Signed-off-by: corphish <d97.avinaba@gmail.com>

--- widgets-ktx/src/main/java/com/corphish/widgets/ktx/viewholders/ClickableViewHolder.kt
+++ widgets-ktx/src/main/java/com/corphish/widgets/ktx/viewholders/ClickableViewHolder.kt
@@ -5,13 +5,9 @@ import android.view.View
 /**
  * Extension of BaseViewHolder with clickable capabilities
  */
-class ClickableViewHolder(view: View, viewList: List<Int>, private val _onClickListener: OnClickListener) : BasicViewHolder(view, viewList), View.OnClickListener {
+class ClickableViewHolder(view: View, viewList: List<Int>, private val _onClickListener: (View, Int) -> Unit) : BasicViewHolder(view, viewList), View.OnClickListener {
     override fun onClick(v: View) {
-        _onClickListener.onClick(v, adapterPosition)
-    }
-
-    interface OnClickListener {
-        fun onClick(view: View?, adapterPosition: Int)
+        _onClickListener(v, adapterPosition)
     }
 
     init {
