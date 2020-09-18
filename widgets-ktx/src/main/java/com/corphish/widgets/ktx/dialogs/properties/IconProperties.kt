a71482b2214ee8d89216a5fe41c2a56a2d7b49cc
corphish <d97.avinaba@gmail.com>
2020-09-18 17:25:42 +0530

    IconProperties: Initial commit
    Signed-off-by: corphish <d97.avinaba@gmail.com>

---
+++ widgets-ktx/src/main/java/com/corphish/widgets/ktx/dialogs/properties/IconProperties.kt
@@ -0,0 +1,32 @@
+package com.corphish.widgets.ktx.dialogs.properties
+
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import androidx.annotation.ColorInt
+
+/**
+ * This class is used define properties of icon.
+ * Exact image to be displayed is not a focus of this class,
+ * but rather the looks of the icon, like the icon color, background
+ * color etc.
+ */
+data class IconProperties(
+        /**
+         * Color of icon.
+         * This will be applied to the foreground of the icon.
+         */
+        @ColorInt val iconColor: Int = Color.WHITE,
+
+        /**
+         * Background color of the icon.
+         * This color will be applied in the background.
+         * By default, platform default is applied.
+         */
+        @ColorInt val backgroundColor: Int = Int.MIN_VALUE,
+
+        /**
+         * Background drawable of the icon.
+         * Use this property to supply custom shapes.
+         */
+        val backgroundDrawable: Drawable? = null
+)
