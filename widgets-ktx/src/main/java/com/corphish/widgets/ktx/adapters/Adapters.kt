f7c235afd62f88798f86b6fb4371137315c80f22
corphish <d97.avinaba@gmail.com>
2020-11-01 22:31:44 +0530

    Adapters: helpers: Update to new immutable list adapter
    Signed-off-by: corphish <d97.avinaba@gmail.com>

--- widgets-ktx/src/main/java/com/corphish/widgets/ktx/adapters/Adapters.kt
+++ widgets-ktx/src/main/java/com/corphish/widgets/ktx/adapters/Adapters.kt
@@ -2,46 +2,71 @@ package com.corphish.widgets.ktx.adapters
 
 import android.view.View
 import android.widget.TextView
-import androidx.annotation.LayoutRes
 import androidx.recyclerview.widget.RecyclerView
 import com.corphish.widgets.ktx.R
 import com.corphish.widgets.ktx.viewholders.BasicViewHolder
+import com.corphish.widgets.ktx.viewholders.ClickableViewHolder
 
 /**
- * Helper class to create adapters
+ * Helper class to create adapters.
  */
 object Adapters {
     /**
-     * This class holds the properties of adapter
-     * @param T Type of item
-     * @param V Type of ViewHolder
+     * This class holds the properties of adapter.
+     *
+     * @param T Type of item.
+     * @param V Type of ViewHolder.
      */
-    class AdapterProperties<T, V: RecyclerView.ViewHolder> {
+    class AdapterProperties<T, V : RecyclerView.ViewHolder> {
         /**
-         * Property for layout resource id
+         * Property for layout resource id.
+         * Now that we can have different layout resource ids for
+         * different type, we need to have a generator for this.
+         *
+         * For demo purposes, we utilise the view type parameter but we
+         * return same layout for every type.
          */
-        @LayoutRes
-        var layoutResourceId: Int = R.layout.layout_item
-
-        /**
-         * Property for listItems
-         */
-        var listItems: List<T> = ArrayList<T>()
-
-        /**
-         * Property for view holder
-         */
-        var viewHolder: (View) -> V = { v -> BasicViewHolder(v, listOf(R.id.item)) as V }
-
-        /**
-         * Property for binding
-         */
-        var binding: (V, T) -> Unit = { vh, it ->
-            (vh as BasicViewHolder).getViewById<TextView>(R.id.item)?.text = (it as String)
+        var layoutResourceId: (Int) -> Int = {
+            when (it) {
+                0 -> R.layout.layout_item
+                else -> R.layout.layout_item
+            }
         }
 
         /**
-         * Property for notifyDataSetChanged
+         * Property for listItems.
+         */
+        var listItems: List<T> = ArrayList()
+
+        /**
+         * Property for view holder.
+         *
+         * For demo purposes, we utilise the type parameter.
+         */
+        var viewHolder: (View, Int) -> V = { v, type ->
+            when (type) {
+                0 -> BasicViewHolder(v, listOf(R.id.item)) as V
+                else -> ClickableViewHolder(v, listOf(R.id.item)) as V
+            }
+        }
+
+        /**
+         * Property for binding.
+         */
+        var binding: (V, Int) -> Unit = { vh, pos ->
+            val type = viewTypeGenerator(pos)
+            val viewHolder = if (type == 0) vh as BasicViewHolder else vh as ClickableViewHolder
+            viewHolder.getViewById<TextView>(R.id.item)?.text = listItems[pos] as String
+        }
+
+        /**
+         * ViewType property.
+         * Returns the view type for given item position.
+         */
+        var viewTypeGenerator: (Int) -> Int = { 0 }
+
+        /**
+         * Property for notifyDataSetChanged.
          */
         var notifyDataSetChanged: Boolean = false
     }
@@ -53,16 +78,18 @@ object Adapters {
      * @param block In this block you can define the properties of ViewHolder
      * @return Static Adapter created based on the set properties
      */
-    fun <T, V: RecyclerView.ViewHolder> newStaticAdapter(block: AdapterProperties<T, V>.() -> Unit): StaticAdapter<T, V> {
+    fun <T, V : RecyclerView.ViewHolder> newImmutableListAdapter(block: AdapterProperties<T, V>.() -> Unit): ImmutableListAdapter<T, V> {
         val props = AdapterProperties<T, V>()
 
         props.block()
 
-        return object: StaticAdaptable<T, V>() {
-            override fun getLayoutResource() = props.layoutResourceId
+        return object : ImmutableListAdaptable<T, V>() {
+            override fun getLayoutResource(viewType: Int) = props.layoutResourceId(viewType)
             override fun getListItems() = props.listItems
-            override fun getViewHolder(view: View) = props.viewHolder(view)
-            override fun bind(viewHolder: V, item: T) { props.binding(viewHolder, item) }
+            override fun getViewHolder(view: View, viewType: Int) = props.viewHolder(view, viewType)
+            override fun bind(viewHolder: V, position: Int) {
+                props.binding(viewHolder, position)
+            }
         }.buildAdapter(props.notifyDataSetChanged)
     }
 
--- widgets-ktx/src/main/java/com/corphish/widgets/ktx/adapters/PrebuiltAdapters.kt
+++ widgets-ktx/src/main/java/com/corphish/widgets/ktx/adapters/PrebuiltAdapters.kt
@@ -6,41 +6,44 @@ import com.corphish.widgets.ktx.R
 import com.corphish.widgets.ktx.viewholders.BasicViewHolder
 
 /**
- * Provides ready-made adapters for use
- * Intended for demo but you can use it nevertheless
+ * Provides ready-made adapters for use.
+ * Intended for demo but you can use it nevertheless.
  */
 object PrebuiltAdapters {
     /**
-     * Returns a recyclerView adapter which can display single line items
-     * @param items Items to display
-     * @return Adapter
+     * Returns a recyclerView adapter which can display single line items.
+     *
+     * @param items Items to display.
+     * @return Adapter.
      */
     fun singleItemAdapterWith(items: List<String>) =
-            object: StaticAdaptable<String, BasicViewHolder>() {
-                override fun getLayoutResource() = R.layout.layout_item
+            object: ImmutableListAdaptable<String, BasicViewHolder>() {
+                override fun getLayoutResource(viewType: Int) = R.layout.layout_item
                 override fun getListItems() = items
-                override fun getViewHolder(view: View) =
+                override fun getViewHolder(view: View, viewType: Int) =
                         BasicViewHolder(view, listOf(R.id.item))
 
-                override fun bind(viewHolder: BasicViewHolder, item: String) {
-                    viewHolder.getViewById<TextView>(R.id.item)?.text = item
+                override fun bind(viewHolder: BasicViewHolder, position: Int) {
+                    viewHolder.getViewById<TextView>(R.id.item)?.text = getListItems()[position]
                 }
             }.buildAdapter(true)
 
     /**
-     * Returns a recyclerView adapter which can display a pair of items
-     * Its layout is same as KeyValueView
-     * @param items Items to display
-     * @return Adapter
+     * Returns a recyclerView adapter which can display a pair of items.
+     * Its layout is same as KeyValueView.
+     *
+     * @param items Items to display.
+     * @return Adapter.
      */
     fun keyValueItemAdapterWith(items: List<Pair<String, String>>) =
-            object: StaticAdaptable<Pair<String, String>, BasicViewHolder>() {
-                override fun getLayoutResource() = R.layout.layout_key_value
+            object: ImmutableListAdaptable<Pair<String, String>, BasicViewHolder>() {
+                override fun getLayoutResource(viewType: Int) = R.layout.layout_key_value
                 override fun getListItems() = items
-                override fun getViewHolder(view: View) =
+                override fun getViewHolder(view: View, viewType: Int) =
                         BasicViewHolder(view, listOf(R.id.key, R.id.value))
 
-                override fun bind(viewHolder: BasicViewHolder, item: Pair<String, String>) {
+                override fun bind(viewHolder: BasicViewHolder, position: Int) {
+                    val item = getListItems()[position]
                     viewHolder.getViewById<TextView>(R.id.key)?.text = item.first
                     viewHolder.getViewById<TextView>(R.id.value)?.text = item.second
                 }
