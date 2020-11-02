67655d3675f3472b827bdb44b88a466352df764f
corphish <d97.avinaba@gmail.com>
2020-11-02 10:09:25 +0530

    app: Update to use new mutable list adapter
    Signed-off-by: corphish <d97.avinaba@gmail.com>

--- app/src/main/java/com/corphish/keyvalueviewtest/MainActivity.kt
+++ app/src/main/java/com/corphish/keyvalueviewtest/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.corphish.keyvalueviewtest
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.corphish.widgets.ktx.adapters.ImmutableListAdaptable
+import androidx.recyclerview.widget.ListAdapter
+import com.corphish.widgets.ktx.adapters.MutableListAdaptable
 import com.corphish.widgets.ktx.viewholders.BasicViewHolder
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,20 +18,19 @@ import kotlinx.android.synthetic.main.activity_main.*
  * Activity for demonstration
  */
 class MainActivity : AppCompatActivity() {
+    private val sampleList = mutableListOf(11, 5, 3, 8, 1/*, 9, 6, 2, 9, 16, 78, 14*/)
+    private lateinit var adapter: ListAdapter<Int, BasicViewHolder>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val sampleList = listOf(11, 5, 3, 8, 1, 9, 6, 2, 9, 16, 78, 14)
-
         recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = object : ImmutableListAdaptable<Int, BasicViewHolder>() {
+        adapter = object : MutableListAdaptable<Int, BasicViewHolder>() {
             override fun getLayoutResource(viewType: Int) = when(viewType) {
                 0 -> R.layout.layout_item_type_0
                 else -> R.layout.layout_item_type_1
             }
-
-            override fun getListItems() = sampleList
 
             override fun getViewHolder(view: View, viewType: Int): BasicViewHolder {
                 return when(viewType) {
@@ -35,15 +39,60 @@ class MainActivity : AppCompatActivity() {
                 }
             }
 
-            override fun bind(viewHolder: BasicViewHolder, position: Int) {
+            override fun bind(viewHolder: BasicViewHolder, item: Int, position: Int) {
                 val textViewId = intArrayOf(R.id.text0, R.id.text1)[getViewType(position)]
-                viewHolder.getViewById<TextView>(textViewId)?.text = "${getListItems()[position]}"
+                viewHolder.getViewById<TextView>(textViewId)?.text = "$item"
             }
 
             override fun getViewType(position: Int) = position % 2
-        }.buildAdapter(true)
+
+            override fun getDiffUtilItemCallback() = object: DiffUtil.ItemCallback<Int>() {
+                override fun areItemsTheSame(oldItem: Int, newItem: Int) = oldItem == newItem
+                override fun areContentsTheSame(oldItem: Int, newItem: Int) = false
+            }
+        }.buildAdapter()
+
+        recyclerView.adapter = adapter
+        adapter.submitList(sampleList)
     }
 
-    val text: String
-        get() = "Success"
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id: Int = item.itemId
+
+        return when (id) {
+            R.id.shuffle -> {
+                adapter.submitList(sampleList.shuffled())
+                true
+            }
+            R.id.add -> {
+                val size = sampleList.size
+                val r1 = (Math.random() * size).toInt()
+                val r2 = (Math.random() * 47).toInt()
+
+                sampleList.add(r1, r2)
+                adapter.submitList(ArrayList(sampleList))
+
+                true
+            }
+            R.id.delete -> {
+                val size = sampleList.size
+                val r1 = (Math.random() * size).toInt()
+
+                sampleList.removeAt(r1)
+                adapter.submitList(sampleList.toList())
+
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
